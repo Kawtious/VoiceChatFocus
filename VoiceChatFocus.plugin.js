7f7c28fa4c261d4b4b385340a8322761088c35da
Z'ark Ashveil <mewmewby2@gmail.com>
2026-05-03 12:29:37 -0700

    Improve code readability

--- VoiceChatFocus.plugin.js
+++ VoiceChatFocus.plugin.js
@@ -9,134 +9,86 @@
  * @updateUrl https://raw.githubusercontent.com/Kawtious/VoiceChatFocus/main/VoiceChatFocus.plugin.js
  */
 
-PLUGIN_ID = "VoiceChatFocus";
+const PLUGIN_ID = "VoiceChatFocus";
 
-UserStore = BdApi.Webpack.Stores.UserStore;
-VoiceStateStore = BdApi.Webpack.Stores.VoiceStateStore;
-MediaEngineStore = BdApi.Webpack.Stores.MediaEngineStore;
+const UserStore = BdApi.Webpack.Stores.UserStore;
+const VoiceStateStore = BdApi.Webpack.Stores.VoiceStateStore;
+const MediaEngineStore = BdApi.Webpack.Stores.MediaEngineStore;
 
-Dispatcher = UserStore._dispatcher;
+const Dispatcher = UserStore._dispatcher;
+
+const FOCUS_BUTTON_STYLE = `
+    @property --vcf-angle {
+        syntax: "<angle>";
+        initial-value: 0deg;
+        inherits: false;
+    }
+
+    @keyframes vcfFadeIn {
+        0% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
+
+    @keyframes vcfSpin {
+        to {
+            --vcf-angle: 360deg;
+        }
+    }
+
+    .vcf-focused::before {
+        content: "";
+        position: absolute;
+        inset: 0;
+        padding: 2px;
+        border-radius: 8px;
+
+        background: conic-gradient(
+            from var(--vcf-angle),
+            transparent 90deg,
+            #b52a37 180deg,
+            #e16741 270deg,
+            transparent 360deg
+        );
+
+        mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
+        mask-composite: exclude;
+
+        animation: vcfFadeIn 1s, vcfSpin 3s linear infinite;
+    }
+`;
 
 module.exports = class VoiceChatFocus {
     constructor() {
         this.focusedUserId = null;
         this.mutedUsers = [];
+        this.voiceStateListener = (payload) => this.onVoiceStateUpdate(payload);
     }
 
     start() {
-        this.unpatch = BdApi.ContextMenu.patch("user-context", (element, context) => {
-            const myId = UserStore.getCurrentUser().id;
+        this.unpatch = BdApi.ContextMenu.patch(
+            "user-context",
+            (element, context) => this.patchUserContext(element, context)
+        );
 
-            // Return if you're right-clicking yourself
-            if (myId === context.user.id) return;
+        Dispatcher.subscribe(
+            "VOICE_STATE_UPDATES",
+            this.voiceStateListener
+        );
 
-            const myVoiceState = VoiceStateStore.getVoiceStateForUser(myId);
-            const userVoiceState = VoiceStateStore.getVoiceStateForUser(context.user.id);
-
-            // Check if both you and the other user are in a voice channel
-            if (!myVoiceState || !userVoiceState) return;
-
-            // Only show if in same voice channel
-            if (myVoiceState.channelId !== userVoiceState.channelId) return;
-
-            // TODO: Is there a better way to do this?
-            //  Supposedly there was a hint here, but these links do not work anymore...
-            //  https://docs.betterdiscord.app/api/utils#findintree
-            //  https://docs.betterdiscord.app/plugins/advanced/react#tree-traversal
-            const menu = element.props.children[0].props.children[4].props.children[0];
-
-            menu.push(
-                BdApi.ContextMenu.buildItem(
-                    {
-                        label: this.focusedUserId !== context.user.id ? "Focus User" : "Unfocus User",
-                        action: () => this.onFocusButtonClick(context.user.id)
-                    }
-                )
-            );
-        });
-
-        this.voiceStateListener = (payload) => {
-            if (!this.focusedUserId) return;
-
-            const voiceState = payload.voiceStates[0];
-
-            // User hasn't left the channel
-            if (!voiceState.oldChannelId) return;
-
-            const myId = UserStore.getCurrentUser().id;
-
-            // Voice state is neither from focused user nor you
-            if (voiceState.userId !== this.focusedUserId && voiceState.userId !== myId) return;
-
-            this.unfocus();
-        };
-
-        Dispatcher.subscribe("VOICE_STATE_UPDATES", this.voiceStateListener);
-
-        BdApi.DOM.addStyle(PLUGIN_ID, `
-            @property --vcf-angle {
-                syntax: "<angle>";
-                initial-value: 0deg;
-                inherits: false;
-            }
-
-            @keyframes vcfFadeIn {
-                0% {
-                    opacity: 0;
-                }
-                100% {
-                    opacity: 1;
-                }
-            }
-
-            @keyframes vcfSpin {
-                to {
-                    --vcf-angle: 360deg;
-                }
-            }
-
-            .vcf-focused::before {
-                content: "";
-                position: absolute;
-                inset: 0;
-                padding: 2px;
-                border-radius: 8px;
-
-                background: conic-gradient(
-                    from var(--vcf-angle),
-                    transparent 90deg,
-                    #b52a37 180deg,
-                    #e16741 270deg,
-                    transparent 360deg
-                );
-
-                mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
-                mask-composite: exclude;
-
-                animation: vcfFadeIn 1s, vcfSpin 3s linear infinite;
-            }
-        `);
+        BdApi.DOM.addStyle(
+            PLUGIN_ID,
+            FOCUS_BUTTON_STYLE
+        );
 
         BdApi.Patcher.after(
             PLUGIN_ID,
             BdApi.Webpack.getByStrings("getProgressForUserId", { defaultExport: false }),
             "Ay",
-            (_, args, returnValue) => {
-                const props = args[0];
-                const userId = props?.user?.id;
-
-                if (!userId || !returnValue?.props) return;
-
-                const className = returnValue.props.className || "";
-
-                if (userId === this.focusedUserId) {
-                    returnValue.props.className = className + " vcf-focused";
-                } else {
-                    returnValue.props.className = className.replace("vcf-focused", "");
-                }
-
-                return returnValue;
-            }
+            (_, args, returnValue) => this.patchVoiceUserElement(args[0], returnValue)
         );
     }
 
@@ -153,6 +105,69 @@ module.exports = class VoiceChatFocus {
         } else {
             this.unfocus();
         }
+    }
+
+    onVoiceStateUpdate(payload) {
+        if (!this.focusedUserId) return;
+
+        const voiceState = payload.voiceStates[0];
+
+        // User hasn't left the channel
+        if (!voiceState.oldChannelId) return;
+
+        const myId = UserStore.getCurrentUser().id;
+
+        // Voice state is neither from focused user nor you
+        if (voiceState.userId !== this.focusedUserId && voiceState.userId !== myId) return;
+
+        this.unfocus();
+    }
+
+    patchUserContext(element, context) {
+        const myId = UserStore.getCurrentUser().id;
+
+        // Return if you're right-clicking yourself
+        if (myId === context.user.id) return;
+
+        const myVoiceState = VoiceStateStore.getVoiceStateForUser(myId);
+        const userVoiceState = VoiceStateStore.getVoiceStateForUser(context.user.id);
+
+        // Check if both you and the other user are in a voice channel
+        if (!myVoiceState || !userVoiceState) return;
+
+        // Only show if in same voice channel
+        if (myVoiceState.channelId !== userVoiceState.channelId) return;
+
+        // TODO: Is there a better way to do this?
+        //  Supposedly there was a hint here, but these links do not work anymore...
+        //  https://docs.betterdiscord.app/api/utils#findintree
+        //  https://docs.betterdiscord.app/plugins/advanced/react#tree-traversal
+        const menu = element.props.children[0].props.children[4].props.children[0];
+
+        menu.push(
+            BdApi.ContextMenu.buildItem(
+                {
+                    label: this.focusedUserId !== context.user.id ? "Focus User" : "Unfocus User",
+                    action: () => this.onFocusButtonClick(context.user.id)
+                }
+            )
+        );
+    }
+
+    patchVoiceUserElement(context, element) {
+        const userId = context?.user?.id;
+
+        if (!userId || !element?.props) return element;
+
+        const className = element.props.className || "";
+
+        if (userId === this.focusedUserId) {
+            element.props.className = className + " vcf-focused";
+        } else {
+            element.props.className = className.replace("vcf-focused", "");
+        }
+
+        return element;
     }
 
     focus(userId) {
